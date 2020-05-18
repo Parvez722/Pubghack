0c1a25d58dd407e600733488bb2e1216c586a2c0
Parvez722 <65571098+Parvez722@users.noreply.github.com>
2020-05-19 04:22:10 +0600

    Add files via upload

---
+++ GL.sh
@@ -0,0 +1,172 @@
+TAG="ཌᴀɴᴏɴʏᴍᴏᴜꜱ ꜱQᴜᴀʀᴅད      "
+echo " "
+fgred=`echo "[31m"`
+YELLOW='[1;33m'
+BLUE='\e[1;94m'
+RED='\e[31m'
+echo " "
+echo "${YELLOW}━━━━━━░⚡░━━━━━━━━━━━━░⚡░━━━━━━━━━━━${BLUE}░🔱░━━━━━━━━━━━░🔱░━━━━━━━━━━━"
+printf "
+${BLUE}     ╔─━━━━━━░${RED}🔱░━━━━━━─╗"
+printf "
+${RED}       ཌᴀɴᴏɴʏᴍᴏᴜꜱ ꜱQᴜᴀʀᴅད      "
+printf "
+${YELLOW}     ཌᴄʀᴇᴀᴛᴏʀ  : ᴀɴᴏɴʏᴍᴏᴜꜱད         "
+printf "
+${RED}       ཌꜱᴜᴘᴘᴏʀᴛ : ꜱᴀʀᴋᴀʀ ད      "
+printf "
+${BLUE}     ╚─━━━━━━░${RED}🔱░━━━━━━─╝"
+echo " "
+echo "${YELLOW}━━━━━━░⚡░━━━━━━━━━━━━░⚡░━━━━━━━━━━━${BLUE}░🔱░━━━━━━━━━━━░🔱░━━━━━━━━━━━"
+sleep 0.5
+echo ""
+echo "${YELLOW} ꜰᴜᴄᴋɪɴɢ ꜱᴛᴀʀᴛ "
+echo -n "\e[1m\e[91m█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ;echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ;echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ;echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█\e[0m\e[0m"
+
+cleanlog()
+{
+chmod 000 /data/data/com.tencent.ig/files/tss_tmp/* > /dev/null 2>&1
+rm -rf /data/data/com.tencent.ig/app_appcache > /dev/null 2>&1
+rm -rf /data/data/com.tencent.ig/app_bugly > /dev/null 2>&1
+rm -rf /data/data/com.tencent.ig/cache > /dev/null 2>&1
+rm -rf /data/data/com.tencent.ig/code_cache > /dev/null 2>&1
+rm -rf /data/data/com.tencent.ig/app_crashrecord > /dev/null 2>&1
+rm -rf /data/data/com.tencent.ig/files/iMSDK > /dev/null 2>&1
+rm -rf /storage/emulated/0/osmini > /dev/null 2>&1
+rm -rf /storage/emulated/0/tencent > /dev/null 2>&1
+rm -rf /storage/emulated/0/MidasOversea > /dev/null 2>&1
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/cache > /dev/null 2>&1
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/tbslog > /dev/null 2>&1
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Logs > /dev/null 2>&1
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/puffer_temp > /dev/null 2>&1
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Pandora > /dev/null 2>&1
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/PufferTmpDir > /dev/null 2>&1
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/RoleInfo > /dev/null 2>&1
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/RoleInfo/RoleInfo.json > /dev/null 2>&1                                                                                                                                                                                                                                                                                                                                                                                                                                             #@PLAYDF25
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Screenshots > /dev/null 2>&1
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/UpdateInfo > /dev/null 2>&1
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/vmpcloudconfig.json > /dev/null 2>&1
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/login-identifier.txt > /dev/null 2>&1
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/cacheFile.txt > /dev/null 2>&1
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/ca-bundle.pem > /dev/null 2>&1
+rm -rf /data/data/com.tencent.ig/files/iMSDK > /dev/null 2>&1
+rm -rf /data/data/com.tencent.ig/files/ss_tmp > /dev/null 2>&1
+rm -rf /data/data/com.tencent.ig/files/AppEventsLogger.persistedevents > /dev/null 2>&1
+rm -rf /data/data/com.tencent.ig/files/tpnlcache.data > /dev/null 2>&1
+rm -rf /data/data/com.tencent.ig/files/tss_app_915c.dat > /dev/null 2>&1
+rm -rf /data/data/com.tencent.ig/files/tss_cs_stat2.dat > /dev/null 2>&1
+rm -rf /data/data/com.tencent.ig/files/tss.i.m.dat > /dev/null 2>&1
+rm -rf /data/data/com.tencent.ig/shared_prefs/TencentUnipay.xml > /dev/null 2>&1
+rm -rf /data/data/com.tencent.ig/shared_prefs/MidasOverseaIP.xml > /dev/null 2>&1
+rm -rf /data/data/com.tencent.ig/shared_prefs/HSJsonData.xml > /dev/null 2>&1
+rm -rf /data/data/com.tencent.ig/shared_prefs/gsdk_prefs.xml > /dev/null 2>&1
+rm -rf /data/data/com.tencent.ig/shared_prefs/crashrecord.xml > /dev/null 2>&1
+rm -rf /data/data/com.tencent.ig/shared_prefs/BuglySdkInfos.xml > /dev/null 2>&1
+rm -rf /data/data/com.tencent.ig/shared_prefs/APMCfg.xml > /dev/null 2>&1
+rm -rf /data/data/com.tencent.ig/shared_prefs/com.google.android.gms.appid.xml > /dev/null 2>&1
+date
+echo " "
+date
+}
+echo ""
+echo ""
+rm -rf /data/data/com.tencent.ig/app_crashrecord
+rm -rf /data/data/com.tencent.ig/cache
+rm -rf /data/data/com.tencent.ig/code_cache
+rm -rf /data/data/com.tencent.ig/files
+rm -rf /data/data/com.tencent.ig/no_backup
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/cache
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Logs
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/UpdateInfo
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/puffer_temp
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/PufferFileList.json
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/puffer_res.eifs
+touch /data/data/com.tencent.ig/app_bugly > /dev/null 2>&1
+touch /data/data/com.tencent.ig/app_crashrecord > /dev/null 2>&1
+touch /data/data/com.tencent.ig/cache > /dev/null 2>&1
+touch /data/data/com.tencent.ig/code_cache > /dev/null 2>&1
+touch /data/data/com.tencent.ig/files > /dev/null 2>&1
+touch /data/data/com.tencent.ig/no_backup > /dev/null 2>&1
+touch /storage/emulated/0/Android/data/com.tencent.ig/cache > /dev/null 2>&1
+touch /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Logs > /dev/null 2>&1
+touch /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/UpdateInfo > /dev/null 2>&1
+touch /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/puffer_temp > /dev/null 2>&1
+mkdir /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/PufferFileList.json
+mkdir /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/puffer_res.eifs
+echo ""
+echo "${BLUE}━━━━━━░⚡░━━━━━━━━━━━━░⚡░━━━━━━━━━━━${YELLOW}░🔱░━━━━━━━━━━━░🔱░━━━━━━━━━━━"
+echo ""
+echo "${BLUE}        ᴄʜᴀɴɢɪɴɢ ɢᴀᴍᴇ ᴅᴀᴛᴀ      ✓"
+echo -n "\e[1m\e[91m█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ;echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ;echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ;echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█\e[0m\e[0m"
+cp -r /sdcard/ᴍᴀɢɪᴄ/game_patch_0.18.0.12600.pak /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/
+cp -r /sdcard/ᴍᴀɢɪᴄ/core_patch_0.18.0.12600.pak /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/
+echo "${YELLOW}        ᴅᴀᴛᴀ ʜᴀꜱ ʙᴇᴇɴ ᴄʜᴀɴɢᴇᴅ     ✓"
+echo ""
+sleep 2
+echo "${BLUE}━━━━━━░⚡░━━━━━━━━━━━━░⚡░━━━━━━━━━━━${YELLOW}░🔱░━━━━━━━━━━━░🔱░━━━━━━━━━━━"
+echo ""
+printf "
+${RED} ꜱᴛᴀʀᴛɪɴɢ ɢᴀᴍᴇ ᴘʟᴇᴀꜱᴇ ᴡᴀɪᴛ..... "
+echo -n "\e[1m\e[91m█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ;echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ;echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ;echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█\e[0m\e[0m"
+sleep 2
+echo " "
+echo "${RED}━━━━━━░⚡░━━━━━━━━━━━━░⚡░━━━━━━━━━━━░🔱░━━━━━━━━━━━░🔱░━━━━━━━━━━━"
+am start -n com.tencent.ig/com.epicgames.ue4.SplashActivity;
+echo "${BLUE}━━━━━━░⚡░━━━━━━━━━━━━░⚡░━━━━━━━━━━━░🔱░━━━━━━━━━━━░🔱░━━━━━━━━━━━"
+chmod 000 /data/data/com.tencent.ig/files/tss_tmp/* &> /dev/null
+sleep 10
+PACKAGE='com.tencent.ig'
+while [ $(pidof $PACKAGE) ]
+do
+rm -rf /data/data/com.tencent.ig/app_bugly 2> /dev/null
+rm -rf /data/data/com.tencent.ig/cache 2> /dev/null
+rm -rf /data/data/com.tencent.ig/code_cache 2> /dev/null
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/cache 2> /dev/null
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/tbslog 2> /dev/null
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Logs 2> /dev/null
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/StatEventReportedFlag 2> /dev/null
+echo " "
+echo -n "\e[1m\e[91m█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ;echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ;echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ;echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█\e[0m\e[0m"
+echo " \n"
+echo " "
+printf "\n${BLUE} ╔${fgred}─${YELLOW}━━━━━━${fgred}░🔱░${BLUE}━━━━━━${fgred}─${YELLOW}╗"
+printf "\n${fgred}   ╭─${YELLOW}──────────${fgred}─╮"
+printf "\n${YELLOW}   ✪⚡     ᴄʟᴇᴀɴᴇᴅ       $c = ᴛɪᴍᴇꜱ"
+printf "\n${fgred}   ╰─${YELLOW}──────────${fgred}─╯ "
+printf "\n${BLUE} ╚${fgred}─${YELLOW}━━━━━━${fgred}░🔱░${BLUE}━━━━━━${fgred}─${YELLOW}╝" 
+echo " "
+printf "\n${BLUE} ╔${fgred}─${YELLOW}━━━━━━${fgred}░🔱░${BLUE}━━━━━━${fgred}─${YELLOW}╗"
+printf "\n${fgred}   ╭─${YELLOW}──────────${fgred}─╮"
+printf "\n${RED}   ✪⚡         ᴠ2.1   ཌᴀɴᴏɴʏᴍᴏᴜꜱ ꜱQᴜᴀʀᴅད "      
+printf "\n${fgred}   ╰─${YELLOW}──────────${fgred}─╯ "
+printf "\n${BLUE} ╚${fgred}─${YELLOW}━━━━━━${fgred}░🔱░${BLUE}━━━━━━${fgred}─${YELLOW}╝" 
+echo " "
+c=$((c+1))
+if [ ! $(pidof $PACKAGE) ]; then
+break
+fi
+sleep 9
+done
+sleep 1
+final=$((c/6))
+printf "\n${fgred}† ʏᴏᴜ ʜᴀᴠᴇ ᴘʟᴀʏᴇᴅ ᴀʟᴍᴏꜱᴛ  ";
+echo $final
+printf "ᴍɪɴᴜᴛᴇ †"
+echo "${YELLOW}━━━━━━░⚡░━━━━━━━━━━━━░⚡░━━━━━━━━━━━${BLUE}░🔱░━━━━━━━━━━━░🔱░━━━━━━━━━━━"
+
+
+echo "  \n   ʀᴇꜱᴛᴏʀɪɴɢ"
+echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ;echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ;echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ;echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ;echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ;echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█" ; sleep .1 ; echo -n "█"
+echo ""
+echo "${YELLOW}━━━━━━░⚡░━━━━━━━━━━━━░⚡░━━━━━━━━━━━${BLUE}░🔱░━━━━━━━━━━━░🔱░━━━━━━━━━━━"
+echo ""
+sleep 1
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/game_patch_0.18.0.12600.pak
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/core_patch_0.18.0.12600.pak
+echo ""
+echo "${YELLOW}━━━━━━░⚡░━━━━━━━━━━━━░⚡░━━━━━━━━━━━${BLUE}░🔱░━━━━━━━━━━━░🔱░━━━━━━━━━━━"
+echo ""
+echo "\n ɢᴀᴍᴇ ʀᴇꜱᴛᴏʀᴇᴅ ꜱᴜᴄᴄᴇꜱꜱꜰᴜʟʟʏ "
+echo "${YELLOW}━━━━━━░⚡░━━━━━━━━━━━━░⚡░━━━━━━━━━━━${BLUE}░🔱░━━━━━━━━━━━░🔱░━━━━━━━━━━━"
+echo "ɢɪᴠᴇ ꜰᴇᴇᴅʙᴀᴄᴋ ᴛᴏ @ᴀɴᴏɴʏᴍᴏᴜꜱ_ɢᴀᴍɪɴɢ"
+echo "${YELLOW}━━━━━━░⚡░━━━━━━━━━━━━░⚡░━━━━━━━━━━━${BLUE}░🔱░━━━━━━━━━━━░🔱░━━━━━━━━━━━"
